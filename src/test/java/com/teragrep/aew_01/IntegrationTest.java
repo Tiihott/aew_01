8f561e24a6e02405cde3c3ec35bb32f92241166e
Tiihott <48@teragrep.com>
2026-06-03 15:05:31 +0300

    Added IntegrationTest

---
+++ src/test/java/com/teragrep/aew_01/IntegrationTest.java
@@ -0,0 +1,162 @@
+/*
+ * RELP sink for Microsoft Azure EventHub (aew_01)
+ * Copyright (C) 2021-2026 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.aew_01;
+
+import com.azure.core.util.IterableStream;
+import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventHubClientBuilder;
+import com.azure.messaging.eventhubs.EventHubConsumerClient;
+import com.azure.messaging.eventhubs.models.EventPosition;
+import com.azure.messaging.eventhubs.models.PartitionEvent;
+import com.teragrep.cnf_01.PropertiesConfiguration;
+import com.teragrep.rlp_01.RelpBatch;
+import com.teragrep.rlp_01.RelpConnection;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.azure.AzuriteContainer;
+import org.testcontainers.azure.EventHubsEmulatorContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.utility.MountableFile;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class IntegrationTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IntegrationTest.class);
+
+    static Network network;
+    static AzuriteContainer azurite;
+    static EventHubsEmulatorContainer eventHubs;
+
+    @BeforeEach
+    void setUp() {
+        network = Network.newNetwork();
+        azurite = new AzuriteContainer("mcr.microsoft.com/azure-storage/azurite:latest").withNetwork(network);
+        azurite.start();
+        eventHubs = new EventHubsEmulatorContainer("mcr.microsoft.com/azure-messaging/eventhubs-emulator:latest")
+                .withConfig(MountableFile.forClasspathResource("eventhubs_config.json"))
+                .acceptLicense()
+                .withNetwork(network)
+                .withAzuriteContainer(azurite);
+        eventHubs.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Assertions.assertDoesNotThrow(eventHubs::stop);
+        Assertions.assertDoesNotThrow(azurite::stop);
+        Assertions.assertDoesNotThrow(network::close);
+    }
+
+    @Test
+    void testRelpAndAmqp() {
+        final String connectionString = eventHubs.getConnectionString();
+        final String connectionStringWithEventHub = connectionString.concat("EntityPath=eh1");
+
+        // Create consumer client to assert that producer works as expected.
+        final EventHubConsumerClient eventHubConsumerClient = new EventHubClientBuilder()
+                .connectionString(eventHubs.getConnectionString())
+                .fullyQualifiedNamespace("emulatorNs1")
+                .eventHubName("eh1")
+                .consumerGroup("cg1")
+                .buildConsumerClient();
+
+        final Properties testProperties = new Properties();
+        testProperties.put("port", "1601");
+        testProperties.put("tls", "false");
+        testProperties.put("tlsKeystorePassword", "changeit");
+        testProperties.put("tlsTruststorePassword", "changeit");
+        final PropertiesConfiguration config = new PropertiesConfiguration(testProperties);
+        final Map<String, String> configurationMap = config.asMap();
+
+        final AMQP amqpClient = new AMQP(connectionStringWithEventHub, "eh1", "emulatorNs1");
+
+        final RELP relp = new RELP(
+                configurationMap,
+                frameContext -> amqpClient
+                        .publishEvents(List.of(new EventData(frameContext.relpFrame().payload().toString())))
+        );
+        Thread relpThread = new Thread(relp);
+        relpThread.start();
+        // Wait for the server to start
+        Assertions.assertDoesNotThrow(() -> Thread.sleep(5 * 1000));
+        // send message to the RELP server.
+        final RelpConnection relpConnection = new RelpConnection();
+        final int port = 1601;
+        Assertions.assertDoesNotThrow(() -> relpConnection.connect("localhost", port));
+        final RelpBatch relpBatch = new RelpBatch();
+        long reqId = relpBatch.insert("Hello World!".getBytes(StandardCharsets.UTF_8));
+        Assertions.assertAll(() -> relpConnection.commit(relpBatch));
+        // verify successful transaction
+        Assertions.assertTrue(relpBatch.verifyTransaction(reqId));
+        Assertions.assertAll(relpConnection::disconnect);
+        relpThread.interrupt();
+
+        final String partitionId = "0";
+        final Instant twelveHoursAgo = Instant.now().minus(Duration.ofHours(12));
+        final EventPosition startingPosition = EventPosition.fromEnqueuedTime(twelveHoursAgo);
+        // Read events from partition '0' and returns the first 100 received or until the 30 seconds has elapsed.
+        final IterableStream<PartitionEvent> events = eventHubConsumerClient
+                .receiveFromPartition(partitionId, 100, startingPosition, Duration.ofSeconds(30));
+
+        final Iterator<PartitionEvent> iterator = events.iterator();
+        Assertions.assertTrue(iterator.hasNext());
+        PartitionEvent first = iterator.next();
+        Assertions.assertEquals("Hello World!", first.getData().getBodyAsString());
+        Assertions.assertFalse(iterator.hasNext());
+        eventHubConsumerClient.close();
+    }
+}
