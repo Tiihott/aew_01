2e70ca764a990045b4a9ce2d91776aec5fe11038
Tiihott <48@teragrep.com>
2026-09-25 14:20:10 +0300

    Added connectionType to AmqpConfig.java

--- src/main/java/com/teragrep/aew_01/config/AmqpConfig.java
+++ src/main/java/com/teragrep/aew_01/config/AmqpConfig.java
@@ -53,13 +53,15 @@ public final class AmqpConfig {
     private final String eventHubName;
     private final String connectionString;
     private final String clientId;
+    private final String connectionType;
 
     public AmqpConfig(final Sourceable configSource) {
         this(
                 configSource.source("azure.namespace", "<NAMESPACE NAME>"),
                 configSource.source("azure.eventhub", "<EVENT HUB NAME>"),
                 configSource.source("azure.connectionString", "<CONNECTION STRING>"),
-                configSource.source("azure.userManagedIdentityClientId", "<USER MANAGED IDENTITY ID>")
+                configSource.source("azure.userManagedIdentityClientId", "<USER MANAGED IDENTITY ID>"),
+                configSource.source("azure.connectionType", "passwordless")
         );
     }
 
@@ -67,12 +69,14 @@ public final class AmqpConfig {
             String eventHubName,
             String fullyQualifiedNamespace,
             String connectionString,
-            String userManagedIdentityClientId
+            String userManagedIdentityClientId,
+            String connectionType
     ) {
         this.eventHubName = eventHubName;
         this.fullyQualifiedNamespace = fullyQualifiedNamespace;
         this.connectionString = connectionString;
         this.clientId = userManagedIdentityClientId;
+        this.connectionType = connectionType;
     }
 
     public String namespaceName() {
@@ -89,5 +93,9 @@ public final class AmqpConfig {
 
     public String userManagedIdentityClientId() {
         return clientId;
+    }
+
+    public String connectionType() {
+        return connectionType;
     }
 }
